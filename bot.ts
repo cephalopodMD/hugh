66b78dfa8830ff121c2b838b54f094fe81788564
cephalopodMD <augustuslogsdon@gmail.com>
2021-10-21 01:14:42 -0400

    basic approval flow

--- bot.ts
+++ bot.ts
@@ -3,7 +3,7 @@ import { Client, Collection, Intents, Message, TextChannel, EmojiIdentifierResol
 // TODO: point this lad to the correct channel - this is just for dev purposes
 const channelID: string = '900566991130206280'
 
-const fakeTweet = (tweet: string) => {
+const fakeTweet = (tweet: any) => {
     console.log(`Fake Tweeting "${tweet}"`);
 }
 
@@ -16,35 +16,45 @@ const client: Client = new Client({
 });
 
 client.on('ready', async () => {
-    console.log(`Logged in as ${client?.user?.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 
     const channel: TextChannel = client.channels.cache.get(channelID) as any;
     const messages:  Collection<string, Message> = await channel.messages.fetch({limit: 100})
     console.log(`Received ${messages.size} messages`);
     // Sort by reaction counts and print messages. Use reacts to track whether Hugh has considered a post
     let sorted = messages
-        .filter(a => !a.reactions.cache.has('🤖') || !a.reactions.cache.get('🤖')!.me)
+        .filter(a => a.author.id !== client.user.id && (!a.reactions.cache.has('🤖') || !a.reactions.cache.get('🤖').me))
         // this should probably be counts of users who reacted or something - this is definitely very wrong!
         .sort((a, b) => b.reactions.cache.size - a.reactions.cache.size)
-        .first(3)
-    console.log('Top 3 unposted by reacts:')
-    sorted.forEach(message => console.log(`  ${message.reactions.cache.size} "${message.content}"`))
+    // see sorted for debugging purposes
+    // console.log('Top 3 unposted by reacts:')
+    // sorted.forEach(message => console.log(`  ${message.reactions.cache.size} "${message.content}"`))
     
-    const msg: Message = sorted[0]!;
+    const msg: Message = sorted.first();
     // React so Hugh won't check this message again
     await msg.react('🤖');
-
-    fakeTweet(msg.content)
+    await msg.reply('Should I post this up, Jimbo?')
 
     // Clear all the old reacts for debugging
     // messages.forEach(m => m.reactions.cache.forEach(r => r.users.remove(client!.user!.id!)))
 });
 
-client.on('message', (msg: Message) => {
-    if (msg.content === 'jim') {
-        msg.reply('jam');
+client.on('messageReactionAdd', async reaction => {
+    if (reaction.message.author.id === client.user.id && 
+        reaction.message.reference && 
+        !reaction.message.reactions.cache.get('🐓')?.me) {
+        const repliedTo: Message = await reaction.message.channel.messages.fetch(reaction.message.reference.messageId);
+        await reaction.message.react('🐓');
+        fakeTweet(repliedTo.content)
     }
-});
+})
+
+// Jim jam for debugging purposes
+// client.on('messageCreate', (msg: Message) => {
+//     if (msg.content === 'jim') {
+//         msg.reply('jam');
+//     }
+// });
 
 // Run dotenv
 require('dotenv').config();
